5ef3e773a549de48966b234054a6f08bdbb19890
dave7995 <dclark@stackpole.com>
2018-07-10 10:02:06 -0400

    Working on Inventory Updating module using XLRD   Testing phase using views3 right now

--- trakberry/views3.py
+++ trakberry/views3.py
@@ -10,7 +10,7 @@ import os
 import smtplib
 from smtplib import SMTP
 import xlrd
-
+#import pandas
 
 def excel_test(request):
 	
@@ -18,15 +18,51 @@ def excel_test(request):
 #	mlist = os.getcwd()
 	
 	# Change to directory where imported inventory.xlsm is located
-	label_link = '/home/file/import1/Inventory/importedxls'
+#		Use this for local testing
+	label_link = '/home/trackberry/file/import1/Inventory/importedxls/'
+#		Use this one for actual server
+#	label_link = '/home/file/import1/Inventory/importedxls'
+	
+	sheet = 'INVENTORY SYSTEM.xlsm'
+	sheet_name = 'PROSUM REPORT'
 	os.chdir(label_link)
 	
+	book = xlrd.open_workbook(sheet)
 	
+	#working = book.sheet_by_index(1)
+	working = book.sheet_by_name(sheet_name)
 	
-	mlist = os.listdir('.')
+	a = working.cell(7,7).value
+	b = working.cell(7,0).value
+	#mlist = book.sheet_names()
+	#mlist.encode('ascii','ignore')
+	
+	#mlist = xl_workbook.nsheets
+	
+	#unicodedata.normalize('NFKD', mlist).encode('ascii','ignore')
+	#mlist = os.listdir('.')
 	
 
 	#mlist = 'Done'
-	return render(request,"test5.html",{'list':mlist})
+	
+	
+	
+#	tx = ' ' + tx
+#	if (tx.find('"'))>0:
+#		#request.session["test_comment"] = tx
+#		#return out(request)
+#		ty = list(tx)
+#		ta = tx.find('"')
+#		tb = tx.rfind('"')
+#		ty[ta] = "'"
+#		ty[tb] = "'"
+#		tc = "".join(ty)
+			
+	#mlist = mlist + 4
+
+
+	
+	
+	return render(request,"test5.html",{'a':a,'b':b})
  
 
