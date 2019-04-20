94a13ffae6fa9687e96b771b64370804982b9840
Dave <dave7995@gmail.com>
2019-04-20 13:49:09 -0400

    Added the Productin view

---
+++ trakberry/views_production.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from trakberry.forms import kiosk_dispForm1,kiosk_dispForm2,kiosk_dispForm3,kiosk_dispForm4, sup_downForm
+from trakberry.views import done
+from views2 import main_login_form
+from views_mod1 import find_current_date
+from trakberry.views2 import login_initial
+from trakberry.views_testing import machine_list_display
+from trakberry.views_vacation import vacation_temp, vacation_set_current, vacation_set_current2, vacation_set_current5
+from django.http import QueryDict
+import MySQLdb
+import json
+import time 
+import smtplib
+from smtplib import SMTP
+from django.core.context_processors import csrf
+from views_routes import direction
+from time import mktime
+from datetime import datetime, date
+from views_db import db_open
+from datetime import datetime
+
+# *********************************************************************************************************
+# MAIN Production View
+# *********************************************************************************************************
+
