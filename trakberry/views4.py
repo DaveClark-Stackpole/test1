fae1cb3930e4732a46672fc645d8c99947b595eb
Dave Clark <dclark@stackpole.ca>
2019-05-05 17:25:56 -0400

    Updated Kiosk for parts and machine to be accurate

--- trakberry/views4.py
+++ trakberry/views4.py
@@ -23,6 +23,63 @@ from views_db import db_open
 from datetime import datetime
 
 
-def ios_test(request):
+def ios_test1(request):
+	# Creates a new backup table of tkb_cycletimes
+	db, cursor = db_open()  
+	
+	cursor.execute("""DROP TABLE IF EXISTS sc_prod_parts_backup""")
+	cursor.execute("""CREATE TABLE IF NOT EXISTS sc_prod_parts_backup LIKE sc_prod_parts""")
+	cursor.execute('''INSERT sc_prod_parts_backup Select * From sc_prod_parts''')
+	db.commit()
+	db.close()
+	return render(request, "kiosk/kiosk_test4.html")	
 
-	return render(request, "kiosk/kiosk_test3.html")	
+def ios_test2(request):
+	# Searches all in sc_production1 unique asset num with machine >1
+	l = 1
+	len_part = 5
+	id_start = 237056
+	db, cur = db_open()
+
+	sql = "SELECT DISTINCT partno FROM sc_production1 where LENGTH(partno) > '%d' and id > '%d' ORDER BY %s %s" %(len_part,id_start,'partno','ASC')
+
+
+#	sql = "SELECT DISTINCT asset_num, machine, partno FROM sc_production1  where LENGTH(asset_num) >'%d' and LENGTH(machine) >'%d' ORDER BY %s %s,%s %s" %(l,l,'asset_num','ASC','id','DESC')
+	cur.execute(sql)
+	tmp = cur.fetchall()
+	tmp2 = tmp[0]
+	db.close()
+	return render(request, "kiosk/kiosk_test4.html",{'tmp':tmp})
+
+def ios_test(request):
+	# Searches all in sc_production1 unique asset num with machine >1
+	l = 1
+	len_part = 5
+	id_start = 237056
+	dte = []
+	db, cur = db_open()
+	sql = "SELECT DISTINCT partno FROM sc_production1 where LENGTH(partno) > '%d' and id > '%d' ORDER BY %s %s" %(len_part,id_start,'partno','ASC')
+#	sql = "SELECT DISTINCT asset_num, machine, partno FROM sc_production1  where LENGTH(asset_num) >'%d' and LENGTH(machine) >'%d' ORDER BY %s %s,%s %s" %(l,l,'asset_num','ASC','id','DESC')
+	cur.execute(sql)
+	tmp = cur.fetchall()
+	tmp2 = tmp[0]
+
+	for x in tmp:
+#		asset = x[0]
+#		machine = x[1]
+		part = x[0]
+		try:
+			aql = "SELECT COUNT(*) FROM sc_prod_parts WHERE parts_no = '%s'" %(part)
+			cur.execute(aql)
+			t2 = cur.fetchall()
+			t3 = t2[0]
+			cnt = t3[0]
+		except:
+			cnt = 0
+		if int(cnt) < 1:
+			cur.execute('''INSERT INTO sc_prod_parts(parts_no) VALUES(%s)''',(part))
+			db.commit()
+		
+	db.close()
+	return render(request, "kiosk/kiosk_test4.html",{'tmp':tmp})
+
